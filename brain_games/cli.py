d839073eacccded4fff8b0d2c08e9b7d95f24a36
emikhaylov <emikhaylov@htsts.ru>
2019-05-08 12:13:14 +0300

    make game engine

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,29 @@
 import prompt
 
+GAME_STEPS = 3
 
-def run():
-    print("Welcome to the Brain Games!", end='\n\n')
+
+def run(game=None):
+    print("Welcome to the Brain Games!")
+    if game:
+        print(game.DESCRIPTION)
+        print()
+
     name = prompt.string("May I have your name? ")
     print("Hello, {}!".format(name))
+    print()
+
+    if not game:
+        return
+
+    for step in range(0, GAME_STEPS):
+        (question, correct_answer) = game.get_question_data()
+        print('Question: {} '.format(question))
+        answer = input('Your answer: ')
+        if answer != correct_answer:
+            print("'{}' is wrong answer ;(. Correct answer was '{}'"
+                  .format(answer, correct_answer))
+            print("Let's try again, {}!".format(name))
+            return
+        print('Correct!')
+    print('Congratulation, {}!'.format(name))
